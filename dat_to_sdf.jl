8a7163f0cafe8cc8a65bae3a850e084f27421088
Julian Kizanis <julianzia@gmail.com>
2022-10-30 03:23:54 -0600

    sdf is working

--- dat_to_sdf.jl
+++ dat_to_sdf.jl
@@ -13,19 +13,21 @@ function Airfoil(address::String)
     file = open(address)
     lines = readlines(file)
     name = popfirst!(lines)
-    splitlines = split.(lines, " "; limit = 2)
+    slines = strip.(lines, [' '])
+    splitlines = split.(slines, " "; limit = 2)
     numpnts = size(splitlines)[1]
 
     points = zeros(size(splitlines, 1), 2)
     upperpoints = 0
     for i in 1:numpnts
+        sp = splitlines[i]
         points[i, :] = parse.(Float64, splitlines[i])
         if points[i, 1] == 0.0
             upperpoints = points[1:i, :]
         end
     end
     if upperpoints == 0
-        return "could not find leading edge"
+        error("could not find leading edge")
     end
     lowerpoints = points[size(upperpoints, 1):end, :]
 
@@ -36,6 +38,15 @@ function Airfoil(address::String)
 
     return Airfoil(name, points, upperpoints, lowerpoints, upperspline, lowerspline)
 end
+
+function rotate(pivot::Vector, point::Vector, θ::Real)
+    s, c = sin(θ), cos(θ)
+    point -= pivot
+    point = [c -s; s c] * point
+    point += pivot
+    return point
+end
+
 
 function findclosest2points(points::Matrix, pos::Vector)
     pnt1index = 0
@@ -73,32 +84,45 @@ function findclosestpoint(spline, c2pnts::Matrix, pos::Vector, maxdist::Float64,
             return pp, dd
         end
     end
-    return [spline(c2pnts[1,1], 1), spline(c2pnts[1,1], 2)], -1
+    return [spline(c2pnts[1,1], 1), spline(c2pnts[1,1], 2)], c2pnts[2,1]
 end
 
-function findvertdist(spline, pos::Vector, maxdist)
+function findvertdist(spline, pos::Vector, maxdist, maxiter)
     t1 = 1
     t2 = size(spline, 1)
     tm = t2/2
 
-    h1 = abs(spline(t1, 1) - pos[1])
-    h2 = abs(spline(t2, 1) - pos[1])
+    x1 = spline(t1, 1)
+    x2 = spline(t2, 1)
 
-    if h1 > h2
-        t1, h1, t2, h2 = t2, h2, t1, h1
+    if x1 > x2
+        t1, x1, t2, x2 = t2, x2, t1, x1
     end
 
-    while abs(h1 - h2) > maxdist
+    for iter in 1:maxiter
         tm = (t1 + t2)/2
-        hm = abs(spline(tm, 1) - pos[1])
+        xm = spline(tm, 1)
 
-        if hm < h1
-            t1, h1, t2, h2 = tm, hm, t1, h1
-        elseif hm < h2
-            t2, h2 = tm, hm
+        if abs(x1 - pos[1]) < maxdist
+            return pos[2] - spline(t1, 2)
+
+        elseif abs(x2 - pos[1]) < maxdist
+            return pos[2] - spline(t2, 2)
+
+        elseif abs(xm - pos[1]) < maxdist
+            return pos[2] - spline(tm, 2)
+
+        elseif x1 < pos[1] < xm
+            t2, x2 = tm, xm
+        elseif xm < pos[1] < x2
+            t1, x1 = tm, xm
+        else
+            error("out of range! (t1, tm, t2) = ($t1, $tm, $t2) , 
+                (x1, xm, x2) = ($x1, $xm, $x2) , pos[1] = $(pos[1])")
         end
     end
-    return spline(tm, 2) - pos[2]
+    error("Never converged! (t1, t2) = ($t1, $t2) , 
+        (x1, x2) = ($x1, $x2) , pos[1] = $(pos[1])")
 end
 
 function sdf(foil::Airfoil, pos::Vector; maxdist::Float64 = 0.001, maxiter::Int = 100)
@@ -108,29 +132,27 @@ function sdf(foil::Airfoil, pos::Vector; maxdist::Float64 = 0.001, maxiter::Int 
 
     if upper
         cpnt, cdist = findclosestpoint(foil.upperspline, u2pnts, pos, maxdist, maxiter)
-
         if 0 <= pos[1] <= 1
-            vdist = findvertdist(foil.upperspline, pos, maxdist)
+            vdist = findvertdist(foil.upperspline, pos, maxdist, maxiter)
             if vdist >= 0
-                return -1#cdist
+                return cdist
             else
-                return 1
+                return -cdist
             end
         else
-            return 1
+            return cdist
         end
     else
         cpnt, cdist = findclosestpoint(foil.lowerspline, l2pnts, pos, maxdist, maxiter)
-
         if 0 <= pos[1] <= 1
-            vdist = findvertdist(foil.upperspline, pos, maxdist)
+            vdist = findvertdist(foil.lowerspline, pos, maxdist, maxiter)
             if vdist <= 0
-                return -1#cdist
+                return cdist
             else
-                return 1
+                return -cdist
             end
         else
-            return 1
+            return cdist
         end
     end
 end
@@ -140,17 +162,13 @@ function sdf_array(foil::Airfoil; n = 16, m = 16, maxdist::Float64 = 1/(n+m), ma
 
     for i in 1:n, j in 1:m
         A[i,j] = sdf(foil, [2i/n-0.5, 2j/m-1]; maxdist, maxiter)
-        # println("     A[i,j] = ", A[i,j])
     end
     return A
 end
 
-function testrun(;n=800, m=800)
-    foil = Airfoil("Airfoils/N/naca6412.dat")
+function testrun(;n=256, m=256)
+    foil = Airfoil("Airfoils/naca/naca6621.dat")
     S = sdf_array(foil; n, m)
-    S[5,5] = -1
-    heatmap(S'; size=(1000,800), fill=true)
-    plot!((foil.points[:,1].+0.5)*n/2, (foil.points[:,2].+1)*m/2)
+    contour(S'; size=(1000,800), fill=(true), layers=6)
+    plot!((foil.points[:,1].+0.5)*n/2, (foil.points[:,2].+1)*m/2; color=:white, linewidth=2)
 end
-
-testrun()
